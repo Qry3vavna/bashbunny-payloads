b84cd76e934290d31cb8a1ac417c48a13dc858a9
qry3vavna <qry3vavna@gmail.com>
2017-03-08 21:10:44 -0800

    Cleaned up some Bash

--- payloads/library/bunny_helpers.sh
+++ payloads/library/bunny_helpers.sh
@@ -12,10 +12,12 @@
 # 2) After ATTACKMODE, we can get target ip address and hostname.
 ################################################################################
 leasefile="/var/lib/dhcp/dhcpd.leases"
-export TARGET_IP=$(cat $leasefile | grep ^lease | awk '{ print $2 }' | sort | uniq)
-export TARGET_HOSTNAME=$(cat $leasefile | grep hostname | awk '{print $2 }' \
-		| sort | uniq | tail -n1 | sed "s/^[ \t]*//" | sed 's/\"//g' | sed 's/;//')
-export HOST_IP=$(cat /etc/network/interfaces.d/usb0 | grep address | awk {'print $2'})
+export TARGET_IP="$(awk '/^lease/{print $2}' $leasefile | sort -u)"
+# Calls less tools, cleaner format, safer wrapper in double quotes in case no match
+export TARGET_HOSTNAME="$(awk '/hostname/{print $2}' $leasefile | tail -n1 | sed 's/^[ \t]*//;s/[\"\;]//g')"
+# Less tools, grab newest entry from bottom with tail no sort needed & cleaned up sed
+export HOST_IP="$(awk '/address/{print $2}' /etc/network/interfaces.d/usb0)"
+# Cleaned up line
 
 ################################################################################
 # Get switch position
@@ -23,9 +25,9 @@ export HOST_IP=$(cat /etc/network/interfaces.d/usb0 | grep address | awk {'print
 ################################################################################
 
 check_switch() {
-	switch1=`cat /sys/class/gpio_sw/PA8/data`
-	switch2=`cat /sys/class/gpio_sw/PL4/data`
-	switch3=`cat /sys/class/gpio_sw/PL3/data`
+	switch1=$(cat /sys/class/gpio_sw/PA8/data)
+	switch2=$(cat /sys/class/gpio_sw/PL4/data)
+	switch3=$(cat /sys/class/gpio_sw/PL3/data)
 	echo "--- switch1 = $switch1, switch2 = $switch2, switch3 = $switch3"
 	if [ "x$switch1" = "x0" ] && [ "x$switch2" = "x1" ] && [ "x$switch3" = "x1" ]; then
 		SWITCH_POSITION="switch1"
